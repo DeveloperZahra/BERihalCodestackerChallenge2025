b405271111c9330e52e4611b980efa859e18f92d
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-10-26 12:27:29 +0400

    test

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace BERihalCodestackerChallenge2025
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(args);  //
 
             // Add services to the container.
 
