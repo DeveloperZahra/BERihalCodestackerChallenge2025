6ba0333395a41082db8cf97f2eeb58f8e66e96f0
riham <riham91.alsiyabi@gmail.com>
2025-10-26 15:31:22 +0400

    Represents a user in the system

---
+++ Model/User.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace BERihalCodestackerChallenge2025.Model
+{
+    [Index(nameof(Username), IsUnique = true)] 
+    [Index(nameof(Email), IsUnique = true)] 
+    public class User // Represents a user in the system
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(64)]
+        public string Username { get; set; } = default!;
+
+        [Required, MaxLength(128), EmailAddress]
+        public string Email { get; set; } = default!;
+
+        [Required] //  PBKDF2/BCrypt
+        public string PasswordHash { get; set; } = default!;
+
+        [Required]
+        public Role Role { get; set; }
+
+        [Required]
+        public Clearance ClearanceLevel { get; set; } = Clearance.low;
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+         
+        public ICollection<Case>? CreatedCases { get; set; }
+        public ICollection<CaseAssignee>? CaseAssignments { get; set; }
+    }
+
+}
