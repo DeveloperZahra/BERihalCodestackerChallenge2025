fc897c41a1ed87fccb7ca2528d76205e302c3115
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-10-27 08:46:07 +0400

    DTO to create a new case

---
+++ DTOs/CaseDTOs.cs
@@ -0,0 +1,15 @@
+﻿namespace BERihalCodestackerChallenge2025.DTOs
+{
+    // DTO to create a new case
+    public class CaseCreateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string AreaCity { get; set; }
+        public string CaseType { get; set; }
+        public string AuthorizationLevel { get; set; } // low | medium | high | critical
+        public List<int>? ReportIds { get; set; } // link existing crime reports
+    }
+
+
+}
