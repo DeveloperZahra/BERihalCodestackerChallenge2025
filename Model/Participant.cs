b89e1b0e6370926c8b5a7294b6aee50bfdd06c51
riham <riham91.alsiyabi@gmail.com>
2025-10-27 08:50:47 +0400

    [Index(nameof(FullName))] // Index on FullName for faster searches

--- Model/Participant.cs
+++ Model/Participant.cs
@@ -1,6 +1,26 @@
-﻿namespace BERihalCodestackerChallenge2025.Model
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BERihalCodestackerChallenge2025.Model
 {
+   
+    // Participants (people, not system users)
+    
+    [Index(nameof(FullName))] // Index on FullName for faster searches
     public class Participant
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(160)]
+        public string FullName { get; set; } = default!;
+
+        [MaxLength(32), Phone]
+        public string? Phone { get; set; }
+
+        [MaxLength(1024)]
+        public string? Notes { get; set; }
+
+        public ICollection<CaseParticipant>? CaseLinks { get; set; }
     }
 }
