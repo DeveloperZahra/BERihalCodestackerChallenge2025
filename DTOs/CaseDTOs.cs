c3cbca59852fb1d5a4965fa4b25b46c0fb76af3f
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-10-27 09:41:25 +0400

    DTO for detailed case info

--- DTOs/CaseDTOs.cs
+++ DTOs/CaseDTOs.cs
@@ -34,4 +34,25 @@
         public string CaseType { get; set; }
         public string AuthorizationLevel { get; set; }
     }
+
+    // DTO for detailed case info
+    public class CaseDetailsDto
+    {
+        public string CaseNumber { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string AreaCity { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CaseType { get; set; }
+        public string CaseLevel { get; set; } // alias for AuthorizationLevel
+        public string AuthorizationLevel { get; set; }
+        public List<string> ReportedBy { get; set; }
+
+        public int NumberOfAssignees { get; set; }
+        public int NumberOfEvidences { get; set; }
+        public int NumberOfSuspects { get; set; }
+        public int NumberOfVictims { get; set; }
+        public int NumberOfWitnesses { get; set; }
+    }
 }
