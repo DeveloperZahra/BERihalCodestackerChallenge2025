25cde5c7ccc88a6d846d7fb43198a231cfeb30e2
riham <riham91.alsiyabi@gmail.com>
2025-10-27 10:20:55 +0400

    create enum

---
+++ Model/Enums/enum.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrimeCase.Api.Models
+{
+    // ---------------------------
+    // Enums
+    // ---------------------------
+    public enum Role { Admin, Investigator, Officer }
+    public enum Clearance { low, medium, high, critical }
+    public enum CaseStatus { pending, ongoing, closed }
+    public enum ReportStatus { pending, en_route, on_scene, under_investigation, resolved }
+    public enum ParticipantRole { suspect, victim, witness }
+    public enum EvidenceType { text, image }
+
+
+}
