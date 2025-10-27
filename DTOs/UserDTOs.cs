597f7a9ef8dbc214670657eea4baccdaf26c318d
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-10-27 08:15:39 +0400

    DTO for creating or updating a user

---
+++ DTOs/UserDTOs.cs
@@ -0,0 +1,14 @@
+﻿namespace BERihalCodestackerChallenge2025.DTOs
+{
+    // DTO for creating or updating a user
+    public class UserCreateUpdateDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; } // Admin | Investigator | Officer
+        public string ClearanceLevel { get; set; } // low | medium | high | critical
+        public string Password { get; set; } // plain password input only
+    }
+
+}
