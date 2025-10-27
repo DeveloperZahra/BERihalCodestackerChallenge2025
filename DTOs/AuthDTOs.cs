96e871542a0a54967682eda2ef5a7f9d066ce7ee
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-10-27 10:03:32 +0400

    Used in login requests (Basic Auth may decode from header instead)

---
+++ DTOs/AuthDTOs.cs
@@ -0,0 +1,9 @@
+﻿namespace BERihalCodestackerChallenge2025.DTOs
+{
+    // Used in login requests (Basic Auth may decode from header instead)
+    public class LoginRequestDto
+    {
+        public string UsernameOrEmail { get; set; }
+        public string Password { get; set; }
+    }
+}
