970f35525bcbda48caaccf041dec700b29e236bd
riham <riham91.alsiyabi@gmail.com>
2025-10-27 10:21:41 +0400

    User roles in the system

--- Model/Enums/enum.cs
+++ Model/Enums/enum.cs
@@ -7,7 +7,7 @@ namespace CrimeCase.Api.Models
     // ---------------------------
     // Enums
     // ---------------------------
-    public enum Role { Admin, Investigator, Officer }
+    public enum Role { Admin, Investigator, Officer } // User roles in the system
     public enum Clearance { low, medium, high, critical }
     public enum CaseStatus { pending, ongoing, closed }
     public enum ReportStatus { pending, en_route, on_scene, under_investigation, resolved }
