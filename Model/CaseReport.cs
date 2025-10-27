a4f0ab2140d5fd3ef7c91755b40b2a1c17ba6030
riham <riham91.alsiyabi@gmail.com>
2025-10-27 08:42:40 +0400

    [Index(nameof(CaseId), nameof(ReportId), IsUnique = true)] // Ensure a report is linked only once per case

--- Model/CaseReport.cs
+++ Model/CaseReport.cs
@@ -1,6 +1,28 @@
-﻿namespace BERihalCodestackerChallenge2025.Model
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations; 
+
+namespace BERihalCodestackerChallenge2025.Model 
 {
-    public class CaseReport
+    [Index(nameof(CaseId), nameof(ReportId), IsUnique = true)] // Ensure a report is linked only once per case
+    public class CaseReport 
     {
+        [Key] 
+        public int Id { get; set; } 
+
+        [Required]
+        public int CaseId { get; set; } 
+
+        [ForeignKey(nameof(CaseId))] 
+        public Case Case { get; set; } = default!; 
+
+        [Required]
+        public int ReportId { get; set; } 
+
+        [ForeignKey(nameof(ReportId))] 
+        public CrimeReport Report { get; set; } = default!; 
+
+        [Required]
+        public DateTime LinkedAt { get; set; } = DateTime.UtcNow; 
     }
 }
