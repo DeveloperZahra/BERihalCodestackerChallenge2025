e50a6dc64d8e1f6794f690d56c64bb3805d85aab
riham <riham91.alsiyabi@gmail.com>
2025-10-28 09:02:19 +0400

    create interface IGenericRepository

--- Repositories/IGenericRepository.cs
+++ Repositories/IGenericRepository.cs
@@ -1,15 +1,15 @@
 ﻿using System.Linq.Expressions;
 
-namespace BERihalCodestackerChallenge2025.Repositories
+namespace BERihalCodestackerChallenge2025.Repositories // Generic repository interface for CRUD operations
 {
-    public interface IGenericRepository<T>
+    public interface IGenericRepository<T> // Generic repository interface for CRUD operations
     {
-        Task AddAsync(T entity);
-        void Delete(T entity);
-        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
-        Task<IEnumerable<T>> GetAllAsync();
-        Task<T?> GetByIdAsync(int id);
-        Task SaveAsync();
-        void Update(T entity);
+        Task AddAsync(T entity); // Add a new entity
+        void Delete(T entity); // Delete an entity
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate); // Find entities matching a given predicate
+        Task<IEnumerable<T>> GetAllAsync(); // Retrieve all entities
+        Task<T?> GetByIdAsync(int id); // Retrieve an entity by its primary key
+        Task SaveAsync(); // Save changes to the database
+        void Update(T entity); // Update an existing entity
     }
 }
