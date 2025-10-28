2a216c7220938454408a661d6c63cfdc021fe42a
riham <riham91.alsiyabi@gmail.com>
2025-10-28 09:46:46 +0400

    create CaseRepository

--- Repositories/IUserRepository.cs
+++ Repositories/IUserRepository.cs
@@ -4,7 +4,7 @@ namespace BERihalCodestackerChallenge2025.Repositories
 {
     public interface IUserRepository // User repository interface for user-specific operations
     {
-        Task<bool> ExistsByEmailAsync(string email); 
-        Task<User?> GetByUsernameOrEmailAsync(string usernameOrEmail); 
+        Task<bool> ExistsByEmailAsync(string email); // Check if a user exists by email
+        Task<User?> GetByUsernameOrEmailAsync(string usernameOrEmail); // Retrieve a user by username or email
     }
 }
