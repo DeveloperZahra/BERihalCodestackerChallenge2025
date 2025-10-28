337ab4261f1e3dec26fb01f0da99c13bafe35c1f
riham <riham91.alsiyabi@gmail.com>
2025-10-28 08:56:51 +0400

    Generic repository implementation for CRUD operations

--- Repositories/GenericRepository.cs
+++ Repositories/GenericRepository.cs
@@ -1,6 +1,43 @@
-﻿namespace BERihalCodestackerChallenge2025.Repositories
+﻿using BERihalCodestackerChallenge2025.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BERihalCodestackerChallenge2025.Repositories
 {
-    public class GenericRepository
+    public class GenericRepository<T> : IGenericRepository<T> where T : class // Generic repository implementation for CRUD operations
+
     {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+
+        public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+            => await _dbSet.Where(predicate).ToListAsync();
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public async Task SaveAsync() => await _context.SaveChangesAsync();
     }
 }
---
+++ Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace BERihalCodestackerChallenge2025.Repositories
+{
+    public interface IGenericRepository<T>
+    {
+        Task AddAsync(T entity);
+        void Delete(T entity);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task SaveAsync();
+        void Update(T entity);
+    }
+}
