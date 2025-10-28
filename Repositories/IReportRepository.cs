9be68d630336d4dcd3966659b4bfbf4d697bbfd4
riham <riham91.alsiyabi@gmail.com>
2025-10-28 10:01:00 +0400

    Repository for crime reports

---
+++ Repositories/IReportRepository.cs
@@ -0,0 +1,9 @@
+﻿using BERihalCodestackerChallenge2025.Model;
+
+namespace BERihalCodestackerChallenge2025.Repositories
+{
+    public interface IReportRepository
+    {
+        Task<CrimeReport?> GetByTrackingCodeAsync(string trackingCode);
+    }
+}
--- Repositories/ReportRepository.cs
+++ Repositories/ReportRepository.cs
@@ -1,6 +1,19 @@
-﻿namespace BERihalCodestackerChallenge2025.Repositories
+﻿using BERihalCodestackerChallenge2025.Data;
+using BERihalCodestackerChallenge2025.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BERihalCodestackerChallenge2025.Repositories
 {
-    public class ReportRepository
+    public class ReportRepository : GenericRepository<CrimeReport>, IReportRepository // Repository for crime reports
     {
+        private readonly AppDbContext _context;
+        public ReportRepository(AppDbContext context) : base(context) => _context = context;
+
+        public async Task<CrimeReport?> GetByTrackingCodeAsync(string trackingCode)
+        {
+            return await _context.CrimeReports
+                .Include(r => r.ReportedByUser)
+                .FirstOrDefaultAsync(r => r.TrackingCode == trackingCode);
+        }
     }
 }
