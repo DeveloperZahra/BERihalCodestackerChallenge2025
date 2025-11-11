813e1edac49e38724018c724f4e753c40925aabd
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-11-11 07:17:19 +0400

    add some dto affiliated to case assignee

--- DTOs/CaseAssigneeCreateDto.cs
+++ DTOs/CaseAssigneeCreateDto.cs
@@ -12,4 +12,29 @@
         public string ClearanceLevel { get; set; } //
 
     }
+
+    public class CaseAssigneeReadDto
+    {
+        public int CaseAssigneeId { get; set; }
+        public int CaseId { get; set; }
+        public int UserId { get; set; }
+        public string? UserFullName { get; set; }
+        public string? UserRole { get; set; }
+        public string ClearanceLevel { get; set; } = default!;
+        public string AssignedRole { get; set; } = default!;
+        public string ProgressStatus { get; set; } = default!;
+        public DateTime AssignedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+
+    public class CaseAssigneeMyAssignmentDto
+    {
+        public int CaseAssigneeId { get; set; }
+        public int CaseId { get; set; }
+        public string? CaseName { get; set; }
+        public string ProgressStatus { get; set; } = default!;
+        public DateTime AssignedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+
 }
